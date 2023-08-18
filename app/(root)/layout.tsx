90eff0eadb0349670b91ac1bdd142ad1c6b4fe9c
Gonzalo <108426175+gonzalo-brandan@users.noreply.github.com>
2023-08-18 07:59:24 +0000

    add Shared layout components

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,6 +3,11 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
 
+import TopBar from '@/components/shared/TopBar'
+import LeftSideBar from '@/components/shared/LeftSideBar'
+import RightSideBar from '@/components/shared/RightSideBar'
+import BottomBar from '@/components/shared/BottomBar'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -18,7 +23,22 @@ export default function RootLayout({
   return (
     <ClerkProvider>
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+       <TopBar/>
+
+       <main>
+        <LeftSideBar />
+
+        <section className="main-container">
+          <div className="w-full max-w-4xl">
+            {children}
+          </div>
+        </section>
+
+        <RightSideBar />
+        </main>  
+        <BottomBar />
+      </body>
     </html></ClerkProvider>
   )
 }
