ff4c9f98e05efddc958d173d5c4fa8bedda0aa74
Gonzalo <108426175+gonzalo-brandan@users.noreply.github.com>
2023-08-23 11:08:38 +0000

    add user model schema with thread and community links

---
+++ lib/models/user.model.ts
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+import { string } from 'zod';
+
+const userSchema = new mongoose.Schema({
+    id: {type: string, required: true},
+    username: {type: string, required: true, unique: true},
+    name: {type: string, required: true},
+    image: String,
+    bio: String,
+    threads: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Thread'
+    }],
+    onboarded: {
+        type: Boolean,
+        default: false,
+    },
+    communities: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Community'
+    }],
+}); 
+
+const User = mongoose.model.User || mongoose.model('User', userSchema);
+
+export default User;
