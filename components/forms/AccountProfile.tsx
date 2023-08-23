2bff4d47d0afb4ef58bbbd598cf99e933cee1b0f
Gonzalo <108426175+gonzalo-brandan@users.noreply.github.com>
2023-08-23 11:06:06 +0000

    integrate Router and Pathname for user profile data update

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -20,6 +20,8 @@ import { ChangeEvent, useState } from "react"
 import { Textarea } from "@/components/ui/textarea"
 import { isBase64Image } from "@/lib/utils"
 import { useUploadThing } from "@/lib/uploadthing"
+import { updateUser } from "@/lib/actions/user.actions"
+import { usePathname, useRouter } from "next/navigation"
 
 interface Props {
     user: {
@@ -37,6 +39,8 @@ const AccountProfile = ({user, btnTitle}: Props) => {
 
   const [files, setFiles] = useState<File[]>([])
   const { startUpload } = useUploadThing("media");
+  const router = useRouter();
+  const pathname = usePathname();
 
   const form = useForm({
         resolver: zodResolver(UserValidation),
@@ -81,7 +85,14 @@ const AccountProfile = ({user, btnTitle}: Props) => {
         }
       }
 
-      //TODO: Update user profile
+      await updateUser({
+        userId: user.id, 
+        username: values.username,
+        name: values.name, 
+        bio: values.bio,
+        image: values.profile_photo,
+        path: pathname,
+      })
 
     }
 
