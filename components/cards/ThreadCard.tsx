aef058c330b8dd68cbeb222dc56eec66742f0d7a
Gonzalo <108426175+gonzalo-brandan@users.noreply.github.com>
2023-08-29 09:49:03 +0000

    add reply, repost and share button

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -59,13 +59,14 @@ const ThreadCard = ({
 
                         <div className="mt-5 flex flex-col gap-3">
                             <div className="flex gap-3.5">
-                                <Image
-                                    src="/assets/heart-gray.svg"
-                                    alt="heart"
-                                    width={24}
-                                    height={24}
-                                    className="cursor-pointer object-contain"
-                                />
+                                <Image src="/assets/heart-gray.svg" alt="heart" width={24} 
+                                height={24} className="cursor-pointer object-contain"/>
+                                <Image src="/assets/reply.svg" alt="reply" width={24} 
+                                height={24} className="cursor-pointer object-contain"/>
+                                <Image src="/assets/repost.svg" alt="repost" width={24} 
+                                height={24} className="cursor-pointer object-contain"/>
+                                <Image src="/assets/share.svg" alt="share" width={24} 
+                                height={24} className="cursor-pointer object-contain"/>
                             </div>
                         </div>
                     </div>
